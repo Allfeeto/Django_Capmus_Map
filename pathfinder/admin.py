15c1aa84991f04cf874c5441965febf05b116fda
Allfeeto <10509090S@mail.ru>
2024-12-15 02:47:42 +0300

    MWP!

--- pathfinder/admin.py
+++ pathfinder/admin.py
@@ -6,15 +6,17 @@ class NodeAdmin(admin.ModelAdmin):
     list_display = ('name', 'x', 'y', 'floor')  # Отображаемые поля
     list_filter = ('floor',)  # Фильтр по этажу
     search_fields = ('name',)  # Поиск по имени узла
+    list_editable = ('x', 'y')  # Позволяем редактировать x и y прямо в списке
 
 @admin.register(Edge)
 class EdgeAdmin(admin.ModelAdmin):
     list_display = ('from_node', 'to_node', 'distance')  # Отображаемые поля
     search_fields = ('from_node__name', 'to_node__name')  # Поиск по узлам
+    list_editable = ('to_node', 'distance')  # Позволяем редактировать to_node и distance прямо в списке
 
 @admin.register(Floor)
-class FloorAdmin(admin.ModelAdmin):
-    list_display = ('number', 'svg')
+class EdgeFloor(admin.ModelAdmin):
+    list_display = ('number', 'svg')  # Отображаемые поля
 
 @admin.register(Endpoint)
 class EndpointAdmin(admin.ModelAdmin):
